06b034e817d22d74182bdf751e9243855ed523ee
niwo <nik.wolfgramm@gmail.com>
2014-09-03 11:00:46 +0200

    more debug output and requeueing for the worker

--- lib/hardcode/worker.rb
+++ lib/hardcode/worker.rb
@@ -1,5 +1,6 @@
 module Hardcode
   require 'sneakers'
+  require 'sneakers/metrics/logging_metrics'
   require 'json'
   require 'fileutils'
 
@@ -9,24 +10,30 @@ module Hardcode
     include Sneakers::Worker
     from_queue :stack_encode
 
+    Sneakers.configure metrics: Sneakers::Metrics::LoggingMetrics.new
+
     def work(msg)
-      job = JSON.parse(msg)
-      source_file = job[:source]
-
-      if File.extname(source_file).match("^\.(mp4|mp3)$") != nil
-        FileUtils.mv(source_file, job[:dest_dir], verbose: true)
-      else
-        puts output = %x[stack-encode encode --no-progress -l #{LOG} '#{source_file}']
-        if $?.success?
-          puts filename = output[/.*>\s(.*)$/, 1]
-          puts "Transcoding successful, deleting source file."
-          FileUtils.mv(File.join(File.dirname(source_file), filename), job[:dest_dir], verbose: true)
-          FileUtils.rm(source_file, verbose: true)
+      begin
+        job = JSON.parse(msg)
+        source_file = job[:source]
+        if File.extname(source_file).match("^\.(mp4|mp3)$") != nil
+          FileUtils.mv(source_file, job[:dest_dir], verbose: true)
         else
-          puts "Error: Transcoding failed."
+          puts output = %x[stack-encode encode --no-progress -l #{LOG} '#{source_file}']
+          if $?.success?
+            puts filename = output[/.*>\s(.*)$/, 1]
+            puts "Transcoding successful, deleting source file."
+            FileUtils.mv(File.join(File.dirname(source_file), filename), job[:dest_dir], verbose: true)
+            FileUtils.rm(source_file, verbose: true)
+          else
+            puts "Error: Transcoding failed."
+          end
         end
+      rescue => e
+        worker_trace e.backtrace
+        requeue!
+        exit 
       end
-
       ack!
     end
 
